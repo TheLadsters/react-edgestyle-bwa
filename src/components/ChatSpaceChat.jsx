ee70aa8f2e2db691533ad3c4ce542e215ed90134
gfelisarta <german.felisarta@jg-corporation.com>
2023-06-26 17:50:00 +0800

    [Backend] Implement Live Chat
    + Import Pusher
    + Import Echo
    + Implement Websocket listener
    + Implement Proper User ID checking

--- src/components/ChatSpaceChat.jsx
+++ src/components/ChatSpaceChat.jsx
@@ -1,153 +1,240 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react'
-import {Grid, Card, Button, Box, TextField, IconButton} from '@mui/material';
-import {Forum, Send, Error, InsertEmoticon, Add} from '@mui/icons-material';
-import {Tabs, TabList, Tab, TabPanel} from '@mui/joy';
-import EscalatePopup from './Modals/EscalatePopup';
-import TemplatePopup from './Modals/TemplatePopup';
-import MessageLeft from './MessageLeft';
-import MessageRight from './MessageRight';
+import React, { useState, useEffect } from "react";
+import {
+  Grid,
+  Card,
+  Button,
+  Box,
+  TextField,
+  IconButton,
+  inputAdornmentClasses,
+} from "@mui/material";
+import { Forum, Send, Error, InsertEmoticon, Add } from "@mui/icons-material";
+import { Tabs, TabList, Tab, TabPanel } from "@mui/joy";
+import EscalatePopup from "./Modals/EscalatePopup";
+import TemplatePopup from "./Modals/TemplatePopup";
+import MessageLeft from "./MessageLeft";
+import MessageRight from "./MessageRight";
 import axiosClient from "../axios-client";
-
+import { useStateContext } from "../context/ContextProvider";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
 export default function ChatSpaceChat() {
-    const [openEscalate, setOpenEscalate] = React.useState(false);
-    const [openTemplate, setOpenTemplate] = React.useState(false);
-    const [messages, setMesssages] = useState([]);
+  const { user } = useStateContext();
+  const [openEscalate, setOpenEscalate] = React.useState(false);
+  const [openTemplate, setOpenTemplate] = React.useState(false);
+  const [messages, setMesssages] = useState([]);
+  const [messageInput, setMessageInput] = React.useState("");
 
-    function openEscalateModal(){
-        setOpenEscalate(true)
-    }
+  const openEscalateModal = () => {
+    setOpenEscalate(true);
+  };
 
-    function closeEscalateModal(){
-        setOpenEscalate(false)
-    }
+  const closeEscalateModal = () => {
+    setOpenEscalate(false);
+  };
 
-    function openTemplateModal(){
-        setOpenTemplate(true)
-    }
+  const openTemplateModal = () => {
+    setOpenTemplate(true);
+  };
 
-    function closeTemplateModal(){
-        setOpenTemplate(false)
-    }
+  const closeTemplateModal = () => {
+    setOpenTemplate(false);
+  };
 
-    useEffect(() => {
-        async function fetchMessages(){
-            try{
-                await axiosClient.get("/get_messages").then(({ data }) => {
-                    setMesssages(data);
-                });
-            }
-            catch(err){
-                console.log(err);
-            }
-        }
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        await axiosClient.get("/get_messages").then(({ data }) => {
+          setMesssages(data);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    // Fetch Messages Initially
+    fetchMessages();
 
+    const pusher = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+      broadcaster: import.meta.env.VITE_BROADCAST_DRIVER,
+      cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+      protocol: "http",
+      encrypted: true,
+      wsHost: "localhost",
+      wsPort: import.meta.env.VITE_PUSHER_PORT,
+      wssport: import.meta.env.VITE_PUSHER_PORT,
+      transports: ["websocket"],
+      forceTLS: false,
+    });
+
+    const echo = new Echo({
+      broadcaster: "pusher",
+      client: pusher,
+    });
+
+    // Your event listeners
+    echo
+      .channel("private_channel")
+      .subscribed(() => {
+        console.log("connected to websocket");
+      })
+      .listen(".NewMessageEvent", (data) => {
+        // Handle the event data
+        console.log("New Message is Received");
         fetchMessages();
-    }, [setMesssages]);
+      })
+      .error((e) => {
+        console.log("Error Receiving ConsoleLogEvent", e);
+      });
 
-    const styles = {
-        card: {
-            borderRadius: '10px', marginTop: '20px', 
-            height: '90vh', marginRight: '10px',
-            padding: '20px 20px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-        },
-        sendAndInfo: {
-            borderRadius: '20px', padding: '10px 15px', backgroundColor: 'RGB(0,128,45)'
-        },
-        escalate: {
-            borderRadius: '20px', backgroundColor: '#68c42c',
-            padding: '10px 10px', marginRight: '5px'
-        },
-        template: {
-            borderRadius: '20px', backgroundColor: '#ba68c8',
-            padding: '10px 10px', marginRight: '5px'
-        },
-        addAndEmoticon: {
-            backgroundColor: '#e8e4e4',
-        },
-        scrollableContent: {
-            maxHeight: 'calc(100vh - 350px)', // Adjust the height as needed
-            overflowY: 'auto',
-            marginTop: '20px',
-          },
+    // Clean up the Echo instance when the component is unmounted
+    return () => {
+      echo.disconnect();
+    };
+  }, []);
+
+  const storeNewMessage = async (inputMessage) => {
+    const payload = {
+      sender_id: user.id,
+      message: inputMessage,
+    };
+
+    try {
+      await axiosClient.post("/add_message", payload).then(({ data }) => {
+        setMessageInput("");
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const messageArea  = messages.map((msg, idx)=> {
-        return (
-            msg.sender_id === 1 ? 
-            <MessageLeft key={idx} message={msg.message} />
-            :
-            <MessageRight key={idx} message={msg.message} />
-    )});
+  const styles = {
+    card: {
+      borderRadius: "10px",
+      marginTop: "20px",
+      height: "90vh",
+      marginRight: "10px",
+      padding: "20px 20px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+    },
+    sendAndInfo: {
+      borderRadius: "20px",
+      padding: "10px 15px",
+      backgroundColor: "RGB(0,128,45)",
+    },
+    escalate: {
+      borderRadius: "20px",
+      backgroundColor: "#68c42c",
+      padding: "10px 10px",
+      marginRight: "5px",
+    },
+    template: {
+      borderRadius: "20px",
+      backgroundColor: "#ba68c8",
+      padding: "10px 10px",
+      marginRight: "5px",
+    },
+    addAndEmoticon: {
+      backgroundColor: "#e8e4e4",
+    },
+    scrollableContent: {
+      maxHeight: "calc(100vh - 350px)", // Adjust the height as needed
+      overflowY: "auto",
+      marginTop: "20px",
+    },
+  };
+
+  const messageArea = messages.map((msg, idx) => {
+    return msg.sender_id !== user.id ? (
+      <MessageLeft key={idx} message={msg.message} />
+    ) : (
+      <MessageRight key={idx} message={msg.message} />
+    );
+  });
 
   return (
     <>
-<Card sx={{ minWidth: 275, height: '100%' }} style={styles.card}>
-      <Tabs>
-        <TabList>
-          <Tab>タイムライン</Tab>
-          <Tab>Timeline</Tab>
-        </TabList>
+      <Card sx={{ minWidth: 275, height: "100%" }} style={styles.card}>
+        <Tabs>
+          <TabList>
+            <Tab>タイムライン</Tab>
+            <Tab>Timeline</Tab>
+          </TabList>
 
-        <TabPanel value={0} sx={styles.scrollableContent}>
-          {/* Japanese Chat translation */}
-          <Grid container direction={'column'}>
-            {messageArea}
+          <TabPanel value={0} sx={styles.scrollableContent}>
+            {/* Japanese Chat translation */}
+            <Grid container direction={"column"}>
+              {messageArea}
+            </Grid>
+          </TabPanel>
+
+          <TabPanel value={1}>
+            {/* English Chat translation */}
+            <Grid>ENGLISH TRANSLATION</Grid>
+          </TabPanel>
+        </Tabs>
+
+        <Grid item>
+          <Grid container>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Enter a message..."
+                style={{ marginBottom: "15px" }}
+                value={messageInput}
+                onChange={(e) => setMessageInput(e.target.value)}
+              />
+            </Grid>
           </Grid>
-        </TabPanel>
 
-        <TabPanel value={1}>
-          {/* English Chat translation */}
-          <Grid>
-            ENGLISH TRANSLATION
-          </Grid>
-        </TabPanel>
-      </Tabs>
+          <Grid container justifyContent="space-between">
+            <Grid item>
+              <IconButton size="large">
+                <Add sx={styles.addAndEmoticon} />
+              </IconButton>
 
-      <Grid item>
-        <Grid container>
-          <Grid item xs={12}>
-            <TextField fullWidth label="Enter a message..." style={{ marginBottom: '15px' }} />
+              <IconButton size="large">
+                <InsertEmoticon sx={styles.addAndEmoticon} />
+              </IconButton>
+            </Grid>
+
+            <Grid item>
+              <Button
+                variant="contained"
+                style={styles.escalate}
+                onClick={openEscalateModal}
+              >
+                <Error style={{ marginRight: "5px" }} />
+                Escalate
+              </Button>
+
+              <Button
+                variant="contained"
+                style={styles.template}
+                onClick={openTemplateModal}
+              >
+                <Forum style={{ marginRight: "5px" }} />
+                Template
+              </Button>
+
+              <Button
+                variant="contained"
+                style={styles.sendAndInfo}
+                onClick={() => storeNewMessage(messageInput)}
+              >
+                Send
+                <Send style={{ marginLeft: "5px" }} />
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
+      </Card>
 
-        <Grid container justifyContent="space-between">
-          <Grid item>
-            <IconButton size="large">
-              <Add sx={styles.addAndEmoticon} />
-            </IconButton>
-
-            <IconButton size="large">
-              <InsertEmoticon sx={styles.addAndEmoticon} />
-            </IconButton>
-          </Grid>
-
-          <Grid item>
-            <Button variant="contained" style={styles.escalate} onClick={openEscalateModal}>
-              <Error style={{ marginRight: '5px' }} />
-              Escalate
-            </Button>
-
-            <Button variant="contained" style={styles.template} onClick={openTemplateModal}>
-              <Forum style={{ marginRight: '5px' }} />
-              Template
-            </Button>
-
-            <Button variant="contained" style={styles.sendAndInfo}>
-              Send
-              <Send style={{ marginLeft: '5px' }} />
-            </Button>
-          </Grid>
-        </Grid>
-      </Grid>
-</Card>
-
-    <EscalatePopup open={openEscalate} handleClose={closeEscalateModal} />
-    <TemplatePopup open={openTemplate} handleClose={closeTemplateModal} />
+      <EscalatePopup open={openEscalate} handleClose={closeEscalateModal} />
+      <TemplatePopup open={openTemplate} handleClose={closeTemplateModal} />
     </>
-  )
+  );
 }
